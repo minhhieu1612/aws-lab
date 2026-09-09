c884fa8bdac280428caf7ae54cbb58c9b4c21a89
minhhieu1612 <calvin.nguyen.dev@gmail.com>
2026-09-09 16:32:01 +0700

    update port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'aws-node-server',
-      script: './server.js',
-      instances: 'max',
-      exec_mode: 'cluster',
+      name: "aws-node-server",
+      script: "./server.js",
+      instances: "max",
+      exec_mode: "cluster",
       autorestart: true,
       watch: false,
-      max_memory_restart: '500M',
+      max_memory_restart: "500M",
       env: {
-        NODE_ENV: 'production',
-        PORT: 3000
-      }
-    }
-  ]
+        NODE_ENV: "production",
+        PORT: 80,
+      },
+    },
+  ],
 };
